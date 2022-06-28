1c9c458bf3968233a8e88b8902e9e9f20b026aad
semyon422 <semyon422@users.noreply.github.com>
2022-06-28 22:32:28 +0500

    Add function just.layout

--- just/init.lua
+++ just/init.lua
@@ -71,6 +71,47 @@ function just.indent(w)
 	just.sameline()
 end
 
+function just.layout(size, _w)
+	local w = {}
+	local x = {}
+
+	local piexls = 0
+	for i = 1, #_w do
+		local v = _w[i]
+		if v ~= "*" and v > 0 then
+			piexls = piexls + _w[i]
+		end
+	end
+
+	local pos = 0
+	local fill = false
+	for i = 1, #_w do
+		local v = _w[i]
+		w[i] = v
+		if v == "*" then
+			w[i] = 0
+			assert(not fill)
+			fill = true
+		elseif v < 0 then
+			w[i] = -v * (size - piexls)
+		end
+		pos = pos + w[i]
+	end
+
+	local rest = size - pos
+	pos = 0
+
+	for i = 1, #_w do
+		if w[i] == 0 then
+			w[i] = rest
+		end
+		x[i] = pos
+		pos = pos + w[i]
+	end
+
+	return x, w
+end
+
 local function clear_table(t)
 	for k in pairs(t) do
 		t[k] = nil
